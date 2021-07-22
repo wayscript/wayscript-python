4aefa779f378f1aff7d28d9a6e323405cefa7305
Jonathan Meier <jonathan@wayscript.com>
2021-07-22 17:23:50 -0400

    Add tests

--- tests/test_context.py
+++ tests/test_context.py
@@ -1,4 +1,42 @@
-from wayscript import context as ws_context
+import pytest
+import responses
+import requests
 
-def test_hello_world():
-    assert "hello, world" == ws_context.hello_world()
+from wayscript import context as ws_context
+from wayscript import settings as ws_settings
+
+
+def test_always_succeeds():
+    """This test should always succeed"""
+    pass
+
+
+@responses.activate
+@pytest.mark.parametrize(
+    "response_key",
+    [("process"), ("event"), ("lair_trigger")],
+)
+def test_process_expanded_detail_methods(patch_client_get_url, processes_detail_expanded_response, response_key):
+    responses.add(responses.GET, patch_client_get_url,
+                  json=processes_detail_expanded_response, status=200)
+
+    callable = getattr(ws_context, f"get_{response_key}")
+    assert callable() == processes_detail_expanded_response[response_key]
+
+@responses.activate
+def test_get_lair(patch_client_get_url, lairs_detail_response):
+    responses.add(responses.GET, patch_client_get_url,
+                  json=lairs_detail_response, status=200)
+
+    assert ws_context.get_lair() == lairs_detail_response
+
+
+@responses.activate
+def test_get_workspace(patch_client_get_url, lairs_detail_response, workspaces_detail_response, monkeypatch):
+    responses.add(responses.GET, patch_client_get_url,
+                  json=workspaces_detail_response, status=200)
+    
+    monkeypatch.setattr(ws_context, "get_lair", lambda *args, **kwargs: lairs_detail_response)
+
+    workspace = ws_context.get_workspace()
+    assert workspace == workspaces_detail_response
---
+++ tests/test_utils.py
@@ -0,0 +1,29 @@
+import pytest
+import responses
+
+from wayscript import settings, utils
+
+@responses.activate
+@pytest.mark.parametrize(
+    "method,_id,expected_subpath",
+    [
+        ("get_workspace_detail", "19cb0a0f-f48c-4191-84b5-ba7be8ceb1a0", "workspaces/19cb0a0f-f48c-4191-84b5-ba7be8ceb1a0"),
+        ("get_lair_detail","334d88dc-f9bf-4d3e-a7a2-0dec1279e4d9", "lairs/334d88dc-f9bf-4d3e-a7a2-0dec1279e4d9"),
+        ("get_process_detail_expanded","378daaa4-0fc3-48da-9f51-f745151dfc08", "processes/378daaa4-0fc3-48da-9f51-f745151dfc08/detail")
+    ],
+)
+def test__get_url_generates_correct_endpoints(method, _id, expected_subpath):
+    """Test that wayscript client's _get_url method generates the correct expected url subpath"""
+    
+    expected_url = f"https://{settings.WAYSCRIPT_HOST}/{expected_subpath}"
+    responses.add(responses.GET, expected_url,
+                  json={}, status=200)
+
+    client = utils.WayScriptClient()
+    callable = getattr(client, method)
+    response = callable(_id)
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == expected_url
+
+
